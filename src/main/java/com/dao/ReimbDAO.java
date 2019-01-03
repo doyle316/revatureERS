4d78fba21ce0362e5b0864f0d0b16152c49fe314
doyle316 <44326918+doyle316@users.noreply.github.com>
2019-01-03 00:21:36 -0500

    Update ReimbDAO.java

--- src/main/java/com/dao/ReimbDAO.java
+++ src/main/java/com/dao/ReimbDAO.java
@@ -11,6 +11,6 @@ public interface ReimbDAO {
 	public ArrayList<Reimbursement> getReimbursementForUser(String ers_username);
 	public int addReimbursement(double amount, String description, String username, String type);
 	public int updateReimbursement(int reimb_id, String admin_name, String newStatus);
-	public int addReimbursementReceipt(double amount, String description, String username, String type, String filePath);
+	//public int addReimbursementReceipt(double amount, String description, String username, String type, String filePath);
 	ArrayList<Reimbursement> getReimbursementForUser(int ers_user_id, boolean getBlob);
 }
